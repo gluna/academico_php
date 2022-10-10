b6562861fed730f650aab95e8b62b185283e66d7
Gustavo Luna Louvem <gllouvem@gmail.com>
2022-10-10 18:41:08 -0300

    inicio do layout

--- index.php
+++ index.php
@@ -20,9 +20,16 @@
       <a href="#" class="w3-bar-item w3-button w3-padding">Tokyo</a>
     </div>
   </div>
-  <div class="w3-panel w3-card-0">
-    <p>London is the most populous city in the United Kingdom,
-      with a metropolitan area of over 9 million inhabitants.</p>
+
+<div class="w3-container w3-content" style="margin-top:20px;width:70%">
+
+    <div class="w3-card-2 w3-padding" >
+
+        <p>London is the most populous city in the United Kingdom,
+          with a metropolitan area of over 9 million inhabitants.</p>
+
+
   </div>
+</div>
 </body>
 </html>
